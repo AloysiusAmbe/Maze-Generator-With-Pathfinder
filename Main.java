77eea30cd3036f81befb8559847459cf117e7b03
Aloysius <ambealoys@gmail.com>
2020-11-27 21:51:25 -0500

    Loads the JFrame and sets some constraints

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import javax.swing.*;
+
+// GREEN - The start node
+// RED - The target node
+// BLUE - Shows the program searching for the target node
+// YELLOW - Displaying a path from the start node to the end node
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Initializing the GUI class and creating a JFrame
+		GUI gui = new GUI();
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(gui);
+		
+	}
+}
