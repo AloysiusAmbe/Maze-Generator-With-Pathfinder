d383cd5f997bf7320081c5976ba6d1cdebf9d860
Aloysius <ambealoys@gmail.com>
2020-11-27 21:52:11 -0500

    Stores the x and y value of each point on the grid

---
+++ Node.java
@@ -0,0 +1,27 @@
+
+public class Node {
+
+    private int x, y;
+
+    public Node (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+}
